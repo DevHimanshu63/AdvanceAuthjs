309cdd8787f95359e37fb6b05cd0e6725f8738be
Himanshu Singh <himanshusinghchauhan@Himanshus-MacBook-Air.local>
2024-10-04 01:19:08 +0530

    signup routes with verification code

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -1,35 +1,39 @@
  import User from '../model/User.model.js'
- import bcrypt from 'bcryptjs';
+ import bcrypt from 'bcrypt';
  import { verificationToken } from '../utils/generateVarificationCode.js'
+import { generateTokenAndSetCookie } from '../utils/generateTokenandSetCookie.js';
  const signup = async(req , res)=>{
     const {name , email , password} = req.body;
+    console.log(req.body);
     try {
         if(!email || !password || !name){
             throw new Error(`All fields must be required`);
         }
-        const userAlreadyExists = await User.findone({ email:email});
+        const userAlreadyExists = await User.findOne({email});
         if(userAlreadyExists){
             return res.status(400).json({message:`User with this email already exists` , success:false});
         }
-        const hashPassword = await bcrypt(password , 10);
-        const verificationToken = verificationToken()
+        const hashPassword = await bcrypt.hash(password , 10);
+        const verificationTokencode = verificationToken()
         const newUser = new User({
             name,
             email,
             password:hashPassword,
-            verificationToken,
+            verificationToken:verificationTokencode,
             verificationTokenExpiresAt: Date.now() + 3600000, // 1 hour
         });
        await newUser.save()
        //jwt generate token and set cookie
-       
-
-    //    return res.status(200).json({message:`user created successfully` , success:true});
+       generateTokenAndSetCookie(res , newUser._id)
+       return res.status(200).json({message:`user created successfully` , success:true , user:{
+        ...newUser._doc,
+        password:undefined,
+       }});
     } catch (error) {
         console.log(error);
         
     }
-    res.send("sign up routes")
+   
 }
 
  const login = async(req , res)=>{
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import {signup , login , logout} from '../controllers/Auth.js'
 
-router.get('/signup' , signup)
+router.post('/signup' , signup)
 router.get('/login' , login)
 router.get('/logout' , logout)
 
